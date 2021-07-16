2b4a2ad1f09ce2c83c269c62c5df0df6b0f14627
Martin Breest <mbs@spreadshirt.net>
2021-07-16 10:18:53 +0200

    Working on blog roll styling.

--- src/components/renderer/blogRoll.tsx
+++ src/components/renderer/blogRoll.tsx
@@ -15,19 +15,23 @@ export const BlogRoll = ({ fields }: TypeBlogRoll) => {
           <h2 className="h0 pt-4 text-3xl font-medium leading-tight text-gray-900">{title}</h2>
           <div className="leading-relaxed text-lg text-gray-700 py-6">{details}</div>
         </div>
-        <div className="w-full flex flex-row">
-          {topPosts.map(function (page, idx) {
-            const content = page.fields.content as TypePageBlogPost;
-            return (
-              <div key={'article-' + idx} className="pr-4 pb-4 md:w-1/3">
-                <img src={content.fields.illustration[0].secure_url} />
-                <div className="font-bold">{page.fields.title}</div>
-                <Link {...{ page: page }}>
-                  <a className="text-yellow-600 underline">Weiterlesen</a>
-                </Link>
-              </div>
-            );
-          })}
+        <div className="w-full grid justify-center">
+          <div className="flex flex-row flex-nowrap justify-start overflow-x-scroll">
+            {topPosts.map(function (page, idx) {
+              const content = page.fields.content as TypePageBlogPost;
+              return (
+                <div
+                  key={'article-' + idx}
+                  className="pl-4 pr-4 pb-4 flex-shrink-0 w-full md:w-1/3">
+                  <img src={content.fields.illustration[0].secure_url} />
+                  <div className="font-bold">{page.fields.title}</div>
+                  <Link {...{ page: page }}>
+                    <a className="text-yellow-600 underline">Weiterlesen</a>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
         </div>
 
         <div className="flex w-full justify-center pt-8 pb-8">
