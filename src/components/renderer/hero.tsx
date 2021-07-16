0e405c09050dd0529f504806fac452193319f922
Martin Breest <mbs@spreadshirt.net>
2021-07-16 16:37:32 +0200

    Adding demo case for not showing content on mobile.

--- src/components/renderer/hero.tsx
+++ src/components/renderer/hero.tsx
@@ -9,11 +9,15 @@ export const Hero = ({ fields }: TypeHeroImage) => {
     <Background {...background.fields}>
       <div className="flex h-80">
         <div className="flex flex-col justify-center items-start md:w-2/3">
-          <h1 className="h0 pt-4 text-4xl font-medium leading-tight text-gray-900 lg:w-7/12">{title}</h1>
-          <div className="leading-relaxed text-lg text-gray-700 py-6">{details}</div>
-          <Cta {...{cta}}/>          
+          <h1 className="h0 pt-4 text-4xl font-medium leading-tight text-gray-900 lg:w-7/12">
+            {title}
+          </h1>
+          <div className="leading-relaxed text-lg text-gray-700 md:py-6 invisible md:visible">
+            {details}
+          </div>
+          <Cta {...{ cta }} />
         </div>
-      </div>     
+      </div>
     </Background>
   );
 };
