a66ed7e6054257437677669c14238fd7e6f99302
Martin Breest <mbs@spreadshirt.net>
2021-07-21 15:39:38 +0200

    Implemented simple table rendering.

--- src/lib/rich-text/render.tsx
+++ src/lib/rich-text/render.tsx
@@ -17,6 +17,41 @@ export const renderRichText = (rtd: any) =>
       [INLINES.HYPERLINK]: PlainHyperlink,
       [INLINES.ASSET_HYPERLINK]: AssetHyperlink,
       [INLINES.ENTRY_HYPERLINK]: () => null, // Ignore entry hyperlink
+      [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+        if (node.data.target.sys.contentType.sys.id === 'table') {
+          const table = node.data.target.fields.table.tableData;
+          console.log();
+          return (
+            <table className="table-auto border border-gray-300 p-2 mt-2 mb-2">
+              <tr>
+                {table &&
+                  table[0].map(function (column, idx) {
+                    return (
+                      <th className="border border-gray-300 p-2" key={'header-' + idx}>
+                        {column}
+                      </th>
+                    );
+                  })}
+              </tr>
+              {table &&
+                table.slice(1).map(function (row, idx) {
+                  return (
+                    <tr key={'rows-' + idx} className={idx % 2 == 0 ? 'bg-gray-100' : ''}>
+                      {row &&
+                        row.map(function (column, idx1) {
+                          return (
+                            <td key={'column-' + idx} className="border border-gray-300 p-2">
+                              {column}
+                            </td>
+                          );
+                        })}
+                    </tr>
+                  );
+                })}
+            </table>
+          );
+        }
+      },
       [BLOCKS.EMBEDDED_ASSET]: EmbeddedAsset,
       [BLOCKS.HEADING_1]: (node, children) => {
         return (
