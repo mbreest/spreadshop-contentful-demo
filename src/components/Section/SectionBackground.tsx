58849ffc9ce7a13e8f3941da1a7431489116e3a9
Seabstian Degner <sede@spreadshirt.net>
2021-08-03 11:38:51 +0200

    i just wanna have fun :tada:

--- src/components/Section/SectionBackground.tsx
+++ src/components/Section/SectionBackground.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const SectionBackground = ({ overlayStyles, backgroundStyles }) => {
   return (
     <div className="sprd-bg__background-image" style={backgroundStyles}>
-      <div className="sprd-bg__overlay" style={overlayStyles}></div>
+      <div className="sprd-bg__overlay" style={overlayStyles}>
+        FooBar
+      </div>
     </div>
   );
 };
