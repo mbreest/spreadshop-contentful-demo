4685a0ec19c3ed2736b84d4b86c1b4ddbf39e054
Seabstian Degner <sede@spreadshirt.net>
2021-08-03 11:43:15 +0200

    Revert "i just wanna have fun :tada:"
    This reverts commit 58849ffc

--- src/components/Section/SectionBackground.tsx
+++ src/components/Section/SectionBackground.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 const SectionBackground = ({ overlayStyles, backgroundStyles }) => {
   return (
     <div className="sprd-bg__background-image" style={backgroundStyles}>
-      <div className="sprd-bg__overlay" style={overlayStyles}>
-        FooBar
-      </div>
+      <div className="sprd-bg__overlay" style={overlayStyles}></div>
     </div>
   );
 };
