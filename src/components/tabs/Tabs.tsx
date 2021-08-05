5b4046ca8425d286168da12c51970a6db3cde1e2
Seabstian Degner <sede@spreadshirt.net>
2021-08-05 15:26:30 +0200

    add tab module

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { isRichText, renderRichText } from '../../lib/rich-text';
 const Tabs = ({ fields }) => {
   const { tab } = fields;
+  const [activeTab, setActiveTab] = useState(0);
 
   return (
     <div className="tabs">
-      <div className="tab__labels">
-        {tab.map((t) => (
-          <button key={t.fields.title} className="tab__label">
+      <div className="tab__labels flex justify-center">
+        {tab.map((t, index) => (
+          <button
+            onClick={() => setActiveTab(index)}
+            key={t.fields.title}
+            className="tab__label px-6 hover:underline hover:font-bold">
             {t.fields.title}
           </button>
         ))}
       </div>
       <div className="tab__descriptions">
-        {tab.map((t) => (
-          <div key={t.title} className="tab__description">
-            {t.fields.description.content.map((content, index) => {
-              return isRichText(content) ? renderRichText(content) : content;
-            })}
-          </div>
-        ))}
+        <div className="tab__description">
+          {tab[activeTab].fields.description.content.map((content) =>
+            isRichText(content) ? renderRichText(content) : content
+          )}
+        </div>
       </div>
     </div>
   );
